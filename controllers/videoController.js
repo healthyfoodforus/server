85747fac4e768e51f482af6e5e075885e07665fc
hermsz <email@hermi.ltd>
2019-04-04 22:04:39 +0700

    modified youtube controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -9,14 +9,14 @@ class VideoController {
   static getVideos(req, res) {
     ax
       .get(`/search?part=snippet&q=${req.params.keyword}&type=${req.params.type}&key=${process.env.YOUTUBE_TOKEN}`)
-      .then(videos => {
-        res.status(200).json(videos)
+      .then(({ data })=> {
+        res.status(200).json(data.items[0])
       })
       .catch(err => {
-        console.log(err.message)
         res.status(400).json(err)
       })
   }
+
 }
 
 module.exports = VideoController;
