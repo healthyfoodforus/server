caa8562ee54432efb151548b6c7c73634f8abcc7
hermsz <email@hermi.ltd>
2019-04-05 10:47:55 +0700

    modiffied youtube routing

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -5,18 +5,17 @@ let ax = axios.create({
 });
 
 class VideoController {
-
   static getVideos(req, res) {
     ax
-      .get(`/search?part=snippet&q=${req.params.keyword}&type=${req.params.type}&key=${process.env.YOUTUBE_TOKEN}`)
+      .get(`/search?part=snippet&q=${req.body.title}&type=video&key=${process.env.YOUTUBE_TOKEN}`)
       .then(({ data })=> {
-        res.status(200).json(data.items[0])
+        console.log(data)
+        res.status(200).json(data.items[0].id.videoId)
       })
       .catch(err => {
         res.status(400).json(err)
       })
   }
-
 }
 
 module.exports = VideoController;
