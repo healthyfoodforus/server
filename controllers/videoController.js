cc62ef933d03411e27aeddceadbfc3b127b6b2e8
William Suryawan <suryawan.william@gmail.com>
2019-04-05 15:40:03 +0700

    add readme, update youtube controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -10,7 +10,10 @@ class VideoController {
     ax
       .get(`/search?part=snippet&q=${req.body.title}&type=video&key=${process.env.YOUTUBE_TOKEN}`)
       .then(({ data })=> {
-        res.status(200).json(data.items[0].id.videoId)
+        console.log('ini info dari youtube ***************------', data.items[0])
+        res.status(200).json({
+          videoId: data.items[0].id.videoId,
+          description: data.items[0].snippet.description})
       })
       .catch(err => {
         res.status(400).json(err)
